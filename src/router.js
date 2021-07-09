2951aa10cd38933af85d9d0065df5bad6c6b8a71
Laurens Verspeek <l.verspeek@gmail.com>
2021-07-09 21:07:17 +0200

    test wake on lan

--- src/router.js
+++ src/router.js
@@ -1,8 +1,19 @@
 const Router = require('@koa/router');
 
 const router = new Router();
+const wol = require('wake_on_lan');
 
 router.get('/', ctx => {
+    const MAC = '44:8A:5B:5C:06:97';
+
+    wol.wake(MAC, function(error) {
+        if (error) {
+            console.log("Could not send wol")
+        } else {
+            console.log("wol packet sent!")
+        }
+    });
+
     ctx.ok("OK");
 });
 
